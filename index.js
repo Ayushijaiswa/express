4e1daca708b7dd8a7d167f22f6feeebccf0be3b6
Ayushijaiswa <ayushijaiswal0209@gmail.com>
2024-07-22 16:18:39 +0530

    used ejs for rendering

--- index.js
+++ index.js
@@ -1,12 +1,46 @@
 const express=require('express');
+const path=require("path");
+
 
 const app=express();
 let port =3000;
+app.set("view engine","ejs");
+
+
+app.set("views",path.join(__dirname,"/views"));
+
+
+
+
+
 app.listen(port,()=>{
     console.log("app is listening")
 })
+app.get("/ig/:username",(req,res)=>{
+    let {username}=req.params;
+    const users=['khushi','ansh',"harsh"];
+    res.render("insta.ejs",{username,users});
+    console.log(username)
+})
+app.get("/insta/:users",(req,res)=>{
+    let {users}=req.params;
+    const instadata=require("./data.json");
+   
+    const data=instadata[users];
+    res.render("instagram.ejs",{data});
+})
+
+
+
+app.get("/roll",(req,res)=>{
+    let dice=Math.floor(Math.random()*6)+1;
+    //pass in the form of object
+    res.render("dice.ejs",{num:dice})
+})
+
+
 app.get('/',(req,res)=>{
-    res.send("hii")
+    res.render("home.ejs")
 })
 //standard response
 
