6ab4a9a37f3de37ce614616a940c0903eafb73dc
Ayushijaiswa <ayushijaiswal0209@gmail.com>
2024-07-22 15:20:47 +0530

    hii final commmit

--- index.js
+++ index.js
@@ -5,3 +5,21 @@ let port =3000;
 app.listen(port,()=>{
     console.log("app is listening")
 })
+app.get('/',(req,res)=>{
+    res.send("hii")
+})
+//standard response
+
+app.post("/",(req,res)=>{
+    res.send("root post")
+})
+app.get("/:username/:id",(req,res)=>{
+    res.send(req.params)
+    console.log(req.params);
+})
+app.get("*",(req,res)=>{
+    res.send("wildcard")
+ })
+ app.get("/search",(req,res)=>{
+    console.log(req.query)
+ })
